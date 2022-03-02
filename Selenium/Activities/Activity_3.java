26d67e82a9793ead60aa654d19dff2485ffbaa8c
sushmib <33720819+sushmib@users.noreply.github.com>
2022-03-02 14:56:30 +0530

    Add files via upload

---
+++ Selenium/Activities/Activity_3.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Activity_3 {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://training-support.net/selenium/simple-form");
+        String title = driver.getTitle();
+        System.out.println("Page title is: " + title);
+        WebElement firstName = driver.findElement(By.id("firstName"));
+        WebElement lastName = driver.findElement(By.id("lastName"));
+        firstName.sendKeys("Saahil");
+        lastName.sendKeys("Sharma");
+        driver.findElement(By.id("email")).sendKeys("test@example.com");
+        driver.findElement(By.id("number")).sendKeys("1234567890");
+        driver.findElement(By.cssSelector(".ui.green.button")).click();
+        driver.close();
+    }
+}
